afc34c26d3d238c6d8e93035234ee1b9694cea26
Salla Haverinen <salla.haverinen@almpartners.fi>
2020-04-28 15:08:34 +0300

    Version 0.7.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="ahjo",
-    version="0.6.1",
+    version="0.7.0",
     author="ALM Partners Oy",
     author_email="aleksi.korpinen@almpartners.fi",
     description="Database deployment framework",
