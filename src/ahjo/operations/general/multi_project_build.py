d1e8a1d90267eb5822e379004b29c7dd0a25b9bf
Henri Tenhunen <henri.tenhunen@almpartners.fi>
2022-10-07 10:20:13 +0300

    Refresh registered_actions in run_multi_project_build
    DP-233

--- src/ahjo/operations/general/multi_project_build.py
+++ src/ahjo/operations/general/multi_project_build.py
@@ -4,7 +4,7 @@ import importlib
 import sys
 from ahjo.database_utilities import create_conn_info, create_sqlalchemy_url
 from ahjo.interface_methods import load_json_conf
-from ahjo.action import execute_action
+from ahjo.action import execute_action, registered_actions
 from ahjo.operation_manager import format_message
 from logging import getLogger
 from logging.config import fileConfig
@@ -31,6 +31,9 @@ def run_multi_project_build(master_config_path):
 
     for ahjo_project in ahjo_projects:
         
+        # Reload master actions & update global variable: registered_actions
+        importlib.reload(ahjo.scripts.master_actions)
+
         # Load project config
         project_path = str(Path(anchor_path, ahjo_project))
         project_actions = ahjo_projects[ahjo_project]["actions"]
@@ -42,22 +45,26 @@ def run_multi_project_build(master_config_path):
         fileConfig(os.path.join(os.path.dirname(ahjo.__file__), 'resources/logger.ini'))
         logger = getLogger('ahjo')
         logger.info('------')
+        logger.info('Building ahjo project: ' + ahjo_project)
 
-        # Import ahjo actions
+        # Import ahjo project actions
         os.chdir(anchor_parent_path_str)
         if os.path.exists(anchor_name + "/" + ahjo_project + '/ahjo_actions.py'):
-            logger.debug(format_message('ahjo_actions found'))
+            logger.debug(format_message(ahjo_project + ' ahjo_actions found'))
             try:
                 sys.path.append(os.getcwd())
                 importlib.import_module(anchor_name + "." + ahjo_project + ".ahjo_actions")
-                logger.info(format_message('Succesfully loaded ahjo_actions'))
+                logger.info(format_message('Succesfully loaded ' + ahjo_project + ' ahjo_actions'))
             except:
-                logger.exception(format_message('Error while loading ahjo_actions'))
+                logger.exception(format_message('Error while loading ' + ahjo_project + ' ahjo_actions'))
                 raise
         else:
-            logger.info(format_message('ahjo_actions not found'))
+            logger.info(format_message(ahjo_project + ' ahjo_actions not found'))
         os.chdir(project_path)
 
         # Run ahjo actions
         for project_action in project_actions:
             execute_action(project_action, project_config_path, master_engine)
+
+            # Delete project specific ahjo action from the global registered_actions
+            registered_actions.pop(project_action, None)
