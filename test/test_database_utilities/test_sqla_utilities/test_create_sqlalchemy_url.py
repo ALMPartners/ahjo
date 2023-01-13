3f319777ae3af711a8d7e1cb1e37d3dd2bc3f52c
Henri Tenhunen <henri.tenhunen@almpartners.fi>
2023-01-13 10:56:45 +0200

    Add unit tests for odbc_connect settings
    DP-285

--- test/test_database_utilities/test_sqla_utilities/test_create_sqlalchemy_url.py
+++ test/test_database_utilities/test_sqla_utilities/test_create_sqlalchemy_url.py
@@ -2,28 +2,43 @@ import ahjo.database_utilities.sqla_utilities as ahjo
 import pytest
 from sqlalchemy.engine.url import URL
 
-CONN_INFO = [{
-    'host': 'localhost',
-    'port': 5432,
-    'server': 'localhost,5432',
-    'database': 'DB_NAME',
-    'driver': 'Postgre SQL Unicode',
-    'dialect': 'postgresql',
-    'username': 'postgres',
-    'password': 'moimoi',
-    'azure_auth': None
-},
+CONN_INFO = [
     {
-    'host': 'localhost',
-    'port': 1433,
-    'server': 'localhost,1433',
-    'database': 'DB_NAME',
-    'driver': 'ODBC Driver 17 for SQL Server',
-    'dialect': 'mssql+pyodbc',
-    'username': 'sa',
-    'password': 'SALA_kala12',
-    'azure_auth': 'ActiveDirectoryPassword'
-}]
+        'host': 'localhost',
+        'port': 5432,
+        'server': 'localhost,5432',
+        'database': 'DB_NAME',
+        'driver': 'Postgre SQL Unicode',
+        'dialect': 'postgresql',
+        'username': 'postgres',
+        'password': 'moimoi',
+        'azure_auth': None
+    },
+    {
+        'host': 'localhost',
+        'port': 1433,
+        'server': 'localhost,1433',
+        'database': 'DB_NAME',
+        'driver': 'ODBC Driver 17 for SQL Server',
+        'dialect': 'mssql+pyodbc',
+        'username': 'sa',
+        'password': 'SALA_kala12',
+        'azure_auth': 'ActiveDirectoryPassword'
+    },
+    {
+        'host': 'localhost',
+        'port': 1433,
+        'server': 'localhost,1433',
+        'database': 'DB_NAME',
+        'driver': 'ODBC Driver 17 for SQL Server',
+        'dialect': 'mssql+pyodbc',
+        'username': 'sa',
+        'password': 'SALA_kala12',
+        'azure_auth': 'ActiveDirectoryPassword',
+        'odbc_trust_server_certificate': 'yes',
+        'odbc_encrypt': 'no'
+    }
+]
 
 
 @pytest.mark.filterwarnings('ignore::sqlalchemy.exc.SADeprecationWarning')
@@ -52,3 +67,35 @@ def test_create_sqlalchemy_url_should_return_url_for_mssql_master_db():
     url = ahjo.create_sqlalchemy_url(conn_info, use_master_db=True)
     assert url.database == 'master'
     assert 'Database=master' in url.query['odbc_connect']
+
+
+def test_create_sqlalchemy_url_should_disable_odbc_trust_server_certificate_by_default():
+    conn_info = CONN_INFO[1]
+    url = ahjo.create_sqlalchemy_url(conn_info, use_master_db=True)
+    odbc_conn_str = url.query["odbc_connect"]
+    indx = odbc_conn_str.find("TrustServerCertificate")
+    assert odbc_conn_str[indx : (indx) + 25] == "TrustServerCertificate=no"
+
+
+def test_create_sqlalchemy_url_should_enable_odbc_encrypt_by_default():
+    conn_info = CONN_INFO[1]
+    url = ahjo.create_sqlalchemy_url(conn_info, use_master_db=True)
+    odbc_conn_str = url.query["odbc_connect"]
+    indx = odbc_conn_str.find("Encrypt")
+    assert odbc_conn_str[indx : (indx) + 11] == "Encrypt=yes"
+
+
+def test_create_sqlalchemy_url_should_enable_odbc_trust_server_certificate():
+    conn_info = CONN_INFO[2]
+    url = ahjo.create_sqlalchemy_url(conn_info, use_master_db=True)
+    odbc_conn_str = url.query["odbc_connect"]
+    indx = odbc_conn_str.find("TrustServerCertificate")
+    assert odbc_conn_str[indx : (indx) + 26] == "TrustServerCertificate=yes"
+
+
+def test_create_sqlalchemy_url_should_disable_odbc_encrypt():
+    conn_info = CONN_INFO[2]
+    url = ahjo.create_sqlalchemy_url(conn_info, use_master_db=True)
+    odbc_conn_str = url.query["odbc_connect"]
+    indx = odbc_conn_str.find("Encrypt")
+    assert odbc_conn_str[indx : (indx) + 10] == "Encrypt=no"
