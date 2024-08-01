9c4a7e0927d205749b3b58088c47341457ea9320
Henri Tenhunen <henri.tenhunen@almpartners.fi>
2024-08-01 13:55:18 +0300

    Log git version info only if it's not null
    AHJO-56

--- src/ahjo/operations/general/git_version.py
+++ src/ahjo/operations/general/git_version.py
@@ -217,9 +217,10 @@ def _get_git_version(connectable: Union[Engine, Connection], git_table_schema: s
 def print_git_version(connectable: Union[Engine, Connection], git_table_schema: str, git_table: str):
     with OperationManager('Checking Git version from database'):
         repository, branch, version = _get_git_version(connectable, git_table_schema, git_table)
-        logger.info(f"Repository: {repository}")
-        logger.info(f"Branch: {branch}")
-        logger.info(f"Version: {version}")
+        if repository and branch and version:
+            logger.info(f"Repository: {repository}")
+            logger.info(f"Branch: {branch}")
+            logger.info(f"Version: {version}")
 
 
 def get_git_hooks_path() -> str:
