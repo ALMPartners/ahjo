555cf6e01e755301655ee30e8265f7e124164160
Henri Tenhunen <henri.tenhunen@almpartners.fi>
2024-09-12 09:43:56 +0300

    Disable temporarily ahjo-upgrade tests
    AHJO-64

--- test/test_operations/test_general/test_upgrade.py
+++ test/test_operations/test_general/test_upgrade.py
@@ -1,10 +1,10 @@
 import pytest
 import copy
+# todo 
+"""
 from ahjo.operations.general.upgrade import get_upgradable_version_actions, validate_version
 
-# todo 
 
-"""
 NON_UPGRADABLE_VERSIONS = ["v3.0.3", "v3.1.0", "v3.1.1", "v3.1.2"]
 CURRENT_VERSION = "v3.1.2"
 UPGRADABLE_VERSIONS = ["v3.1.3", "v3.1.4", "v3.1.5"]
